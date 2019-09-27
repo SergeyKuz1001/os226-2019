84ce4e712035fe5c5c5389e7853b36402421a490
Kuzivanov Sergey <43182277+SergeyKuz1001@users.noreply.github.com>
2019-09-27 21:57:48 +0300

    Revert "Delete init.c"
    This reverts commit fdab964646f7f66ea7b6e811a67802ebf5e67778.

---
+++ syscall/init.c
@@ -0,0 +1,50 @@
+#define _GNU_SOURCE
+
+#include <stdint.h>
+#include <signal.h>
+#include "init.h"
+#include <ucontext.h>
+#include <sys/ucontext.h>
+#include <stdlib.h>
+
+int _base = 0;
+
+void sig_func(int sig, siginfo_t *info, void *ctx) {
+    ucontext_t *uc = (ucontext_t *) ctx;
+    static int amt_call = 0;
+    if (0x138b == *(uint16_t *) uc->uc_mcontext.gregs[REG_RIP]) {
+        uc->uc_mcontext.gregs[REG_RDX] = 100000 + (++amt_call);
+        uc->uc_mcontext.gregs[REG_RIP] += 2;
+    }
+    else if (0x0000538b == ((*(uint32_t *) uc->uc_mcontext.gregs[REG_RIP]) & 0x0000ffff)) {
+        int index = ((*(uint32_t *) uc->uc_mcontext.gregs[REG_RIP]) & 0x00ff0000) >> 18;
+        uc->uc_mcontext.gregs[REG_RDX] = 100000 + 1000 * index + (++amt_call);
+        uc->uc_mcontext.gregs[REG_RIP] += 3;
+    }
+    else if (0x0000558b == ((*(uint32_t *) uc->uc_mcontext.gregs[REG_RIP]) & 0x0000ffff)) {
+        int index = (((*(uint32_t *) uc->uc_mcontext.gregs[REG_RIP]) & 0x00ff0000) >> 18) + \
+			(((int)(uc->uc_mcontext.gregs[REG_RBP]) - _base) >> 2);
+        uc->uc_mcontext.gregs[REG_RDX] = 100000 + 1000 * index + (++amt_call);
+        uc->uc_mcontext.gregs[REG_RIP] += 3;
+    }
+    else if (0x0b8b == *(uint16_t *) uc->uc_mcontext.gregs[REG_RIP]) {
+        uc->uc_mcontext.gregs[REG_RCX] = 100000 + (++amt_call);
+        uc->uc_mcontext.gregs[REG_RIP] += 2;
+    }
+    else if (0x00004d8b == ((*(uint32_t *) uc->uc_mcontext.gregs[REG_RIP]) & 0x0000ffff)) {
+        int index = (((*(uint32_t *) uc->uc_mcontext.gregs[REG_RIP]) & 0x00ff0000) >> 18) + \
+			(((int)(uc->uc_mcontext.gregs[REG_RBP]) - _base) >> 2);
+        uc->uc_mcontext.gregs[REG_RCX] = 100000 + 1000 * index + (++amt_call);
+        uc->uc_mcontext.gregs[REG_RIP] += 3;
+    }
+}
+
+void init(void *base) {
+    struct sigaction sigact = {
+        .sa_sigaction = sig_func,
+        .sa_flags = SA_RESTART,
+    };
+    sigemptyset(&sigact.sa_mask);
+    sigaction(SIGSEGV, &sigact, NULL);
+	_base = base;
+}
