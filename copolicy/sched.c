991295b748fa9583db3889e5ec05bf3855733773
Kuzivanov Sergey <43182277+SergeyKuz1001@users.noreply.github.com>
2019-09-28 10:11:16 +0300

    Revert "Update sched.c"
    This reverts commit 9b7d4b48e9060f69a07c17b17469ab37a4e3dc65.

--- copolicy/sched.c
+++ copolicy/sched.c
@@ -31,7 +31,6 @@ void sched_new(void (*entrypoint)(void *aspace), void *aspace, int priority, int
 
 void sched_cont(void (*entrypoint)(void *aspace), void *aspace, int timeout) {
     running_app->func = entrypoint;
-	running_app->aspace = aspace;
     running_app->time_continue = time + timeout;
 }
 
