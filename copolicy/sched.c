30d35cc02bc59dab62a654eb0376608e4c7f306d
Kuzivanov Sergey <43182277+SergeyKuz1001@users.noreply.github.com>
2019-09-29 15:48:48 +0300

    Update sched.c

--- copolicy/sched.c
+++ copolicy/sched.c
@@ -26,6 +26,29 @@ void sched_new(void (*entrypoint)(void *aspace), void *aspace, int priority, int
     apps[apps_n].priority = priority;
     apps[apps_n].deadline = deadline;
     apps[apps_n].aspace = aspace;
+
+    if (!policy) {
+        printf("Error: policy was not defined.\n");
+        return;
+    }
+
+    struct sched_app *a = head_queue;
+    struct sched_app *pred_a = 0;
+    while (a) {
+        if (policy == 2 && a->priority > apps[apps_n].priority)
+            break;
+        if (policy == 3 && (a->deadline > apps[apps_n].deadline || \
+                a->deadline == apps[apps_n].deadline && a->priority > apps[apps_n].priority))
+            break;
+        pred_a = a;
+        a = a->next_app;
+	}
+    if (pred_a)
+        pred_a->next_app = apps + apps_n;
+    else
+        head_queue = apps + apps_n;
+    apps[apps_n].next_app = a;
+
     apps_n++;
 }
 
@@ -56,30 +79,7 @@ void sched_set_policy(const char *name) {
 }
 
 void sched_run(void) {
-    if (!policy) {
-        printf("Error: policy was not give.\n");
-        return;
-    }
-
-    for (int i = 0; i < apps_n; i++) {
-        struct sched_app *a = head_queue;
-        struct sched_app *pred_a = 0;
-        while (a) {
-            if (policy == 2 && a->priority > apps[i].priority)
-                break;
-            if (policy == 3 && (a->deadline > apps[i].deadline || a->deadline == apps[i].deadline && a->priority > apps[i].priority))
-                break;
-            pred_a = a;
-            a = a->next_app;
-        }
-        if (pred_a)
-            pred_a->next_app = apps + i;
-        else
-            head_queue = apps + i;
-        apps[i].next_app = a;
-    }
-
-    while (head_queue) {
+   while (head_queue) {
         int min_time_continue = head_queue->time_continue;
         struct sched_app *pred_running_app = 0;
         running_app = head_queue;
@@ -88,14 +88,17 @@ void sched_run(void) {
                 break;
             if (policy == 2 && running_app->next_app && running_app->next_app->priority > running_app->priority)
                 break;
-            if (policy == 3 && running_app->next_app && running_app->next_app->deadline > running_app->deadline)
+            if (policy == 3 && running_app->next_app && \
+                    (running_app->next_app->deadline > running_app->deadline || \
+                    (running_app->next_app->deadline == running_app->deadline && \
+                    running_app->next_app->priority > running_app->priority)))
                 break;
             if (min_time_continue > running_app->time_continue)
                 min_time_continue = running_app->time_continue;
             pred_running_app = running_app;
             running_app = running_app->next_app;
         }
-        if (!running_app) {
+        if (!running_app || running_app->time_continue > time) {
             time = min_time_continue;
             continue;
         }
